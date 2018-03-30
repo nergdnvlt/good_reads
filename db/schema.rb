5e2b02e944200894ab2e5ed9e1167bda9a0d675a
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-30 10:27:22 -0600

    Finished setting up user book relationship.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330162355) do
+ActiveRecord::Schema.define(version: 20180330162622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,9 @@ ActiveRecord::Schema.define(version: 20180330162355) do
     t.string "body"
     t.integer "rating"
     t.bigint "book_id"
+    t.bigint "user_id"
     t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 20180330162355) do
   end
 
   add_foreign_key "reviews", "books"
+  add_foreign_key "reviews", "users"
 end
