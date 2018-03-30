f279a143d8d2edd20190e3e2030fff507ebb9108
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-30 10:24:30 -0600

    Building relationship between book and review.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024183246) do
+ActiveRecord::Schema.define(version: 20180330162355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,16 @@ ActiveRecord::Schema.define(version: 20171024183246) do
     t.string "title"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "body"
+    t.integer "rating"
+    t.bigint "book_id"
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
   end
 
+  add_foreign_key "reviews", "books"
 end
---
+++ spec/features/books/user_sees_all_review_for_book_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'user visits book page to see title and reviews' do
+  scenario 'user is able to see the reviews for a book' do
+    book = Book.create!(title: 'Fellowship of the Ring')
+    user1 = User.create!(name: 'Odin')
+    user2 = User.create!(name: 'Thor')
+    review1 = book.reviews.create!(body: 'Great', user_id: user1.id, rating: 5)
+    review2 = book.reviews.create!(body: 'Ok', user_id: user2.id, rating: 4)
+
+    visit book_path(book)
+
+    expect(page).to have_content(book.title)
+    expect(page).to have_content(review1.body)
+    expect(page).to have_content(review1.user_id)
+    expect(page).to have_content(review1.rating)
+    expect(page).to have_content(review2.body)
+    expect(page).to have_content(review2.user_id)
+    expect(page).to have_content(review2.rating)
+  end
+end
