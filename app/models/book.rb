5e733cbd91b0472ff676a8814ea541cc89ae2c38
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-30 10:51:27 -0600

    Added ugly average method (should use .sum or .inject but just going for functionality.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ApplicationRecord
   has_many :reviews
+
+  def average_review
+    sum = 0
+    reviews.each do |review|
+      sum += review.rating
+    end
+    sum.to_f / reviews.count
+  end
 end
--- spec/features/books/user_visit_showpage_spec.rb
+++ spec/features/books/user_visit_showpage_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'user visits book page to see title and reviews' do
+describe 'user visits book page' do
   scenario 'user is able to see the reviews for a book' do
     book = Book.create!(title: 'Fellowship of the Ring')
     user1 = User.create!(name: 'Odin')
@@ -17,5 +17,17 @@ describe 'user visits book page to see title and reviews' do
     expect(page).to have_content(review2.body)
     expect(page).to have_content(user2.name)
     expect(page).to have_content(review2.rating)
+  end
+
+  scenario 'user is able to see average review' do
+    book = Book.create!(title: 'Fellowship of the Ring')
+    user1 = User.create!(name: 'Odin')
+    user2 = User.create!(name: 'Thor')
+    review1 = book.reviews.create!(body: 'Great', user_id: user1.id, rating: 5)
+    review2 = book.reviews.create!(body: 'Ok', user_id: user2.id, rating: 4)
+
+    visit book_path(book)
+
+    expect(page).to have_content(4.5)
   end
 end
